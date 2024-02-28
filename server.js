b9cf6556bbb4b8a30872dc5918e998a53819d6d7
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-02-28 10:15:25 +0100

    Pushing all persons
    Added better design colors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('public'))
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
   // Haal alle personen uit de FDND API op
-  fetchJson(apiUrl + '/person/?filter={"squad_id":5}').then((data) => {
+  fetchJson(apiUrl + '/person/').then((data) => {
     console.log(data)
     // Render index.ejs uit de views map en geef uit FDND API opgehaalde data mee
     response.render('index', data)
