cb2c663a291681fae9840e5194ae4852b637b715
Robin <robin.django@gmail.com>
2024-02-28 12:19:15 +0100

    Selected filter maintained on reload

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
-const text = document.querySelector('article p');
-const textContent = text.innerHTML;
+const textEl = document.querySelector('article p');
+const textContent = textEl.innerHTML;
 const textArray = textContent.split(/(\s+)/); // Use regex to split while preserving spaces
+const query = new URLSearchParams(window.location.search);
+const filterEl = document.querySelector('select.filter');
 
 textArray.forEach((word, index) => {
     if (word.includes('#')) {
@@ -9,5 +11,24 @@ textArray.forEach((word, index) => {
     }
 });
 
-text.innerHTML = textArray.join(''); // Join without spaces
+if (query.get('filter')) {
+    switch (query.get('filter')) {
+      case "":
+        filterEl.selectedIndex = 1
+        break;
+      case "all":
+        filterEl.selectedIndex = 1
+        break;
+      case "3":
+        filterEl.selectedIndex = 2
+        break;
+      case "4":
+        filterEl.selectedIndex = 3
+        break;
+      case "5":
+        filterEl.selectedIndex = 4
+        break;
+    }
+};
 
+textEl.innerHTML = textArray.join(''); // Join without spaces
